65bfb197177c6753ee5e934ef86c755cc65da237
Davin Smith <davinsmith@outlook.com>
2020-12-05 02:06:25 -0600

    Added 4.1 to TypeScript

--- 04/index.ts
+++ 04/index.ts
@@ -1,6 +1,19 @@
 import { fileToStringArray } from '../utils.js';
 
-const FILE_PATH = 'inputs/04.1.txt';
+const FILE_PATH = '../../inputs/04.1.txt';
+const REQUIRED_FIELDS = [
+  'byr', //(Birth Year)
+  'iyr', //(Issue Year)
+  'eyr', //(Expiration Year)
+  'hgt', //(Height)
+  'hcl', //(Hair Color)
+  'ecl', //(Eye Color)
+  'pid', //(Passport ID)
+  // 'cid', //(Country ID)     // not required
+];
+const VALID_HEIGHT_UNITS = ['cm', 'in'];
+const VALID_EYE_COLORS = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
 interface IPassport {
   byr?: number;
   iyr?: number;
@@ -10,7 +23,6 @@ interface IPassport {
   ecl?: string;
   pid?: string;
 }
-
 interface IPassportValidation {
   byr?: boolean;
   iyr?: boolean;
@@ -21,7 +33,7 @@ interface IPassportValidation {
   pid?: boolean;
 }
 
-const tokenize = (fileLine: string) => fileLine.split('').map((kvPair) => kvPair.split(':'));
+const tokenize = (fileLine: string) => fileLine.split(' ').map((kvPair) => kvPair.split(':'));
 
 const passportsFromInputLines = (inputLines: string[]) =>
   inputLines.reduce(
@@ -36,7 +48,12 @@ const passportsFromInputLines = (inputLines: string[]) =>
     [{}]
   );
 
-const validatePassport = ({ byr, iyr, eyr, hgt, hcl, ecl, pid }: IPassport) => {
+const validatePassportFirst = (passport: IPassport) => {
+  const fieldScoreCard = REQUIRED_FIELDS.map((x) => !!passport[x]);
+  return fieldScoreCard.filter((x) => !x).length === 0;
+};
+
+const validatePassportSecond = ({ byr, iyr, eyr, hgt, hcl, ecl, pid }: IPassport) => {
   const validations: IPassportValidation = {};
 
   try {
@@ -46,17 +63,17 @@ const validatePassport = ({ byr, iyr, eyr, hgt, hcl, ecl, pid }: IPassport) => {
 
     const hgtUnits = hgt.slice(-2);
     const hgtValue = Number(hgt.substring(0, hgt.length - 2));
-    if (['cm', 'in'].includes(hgtUnits)) {
+    if (VALID_HEIGHT_UNITS.includes(hgtUnits)) {
       validations.hgt = hgtUnits === 'cm' ? hgtValue >= 150 && hgtValue <= 193 : hgtValue >= 59 && hgtValue <= 76;
     } else {
       validations.hgt = false;
     }
 
     validations.hcl = !!hcl.match(/^#[0-9a-f]{6}$/i);
-    validations.ecl = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(ecl);
+    validations.ecl = VALID_EYE_COLORS.includes(ecl);
     validations.pid = !!pid.match(/^\d{9}$/);
 
-    return Object.entries(validations).reduce((acc, [key, value]) => acc && value, true);
+    return Object.entries(validations).reduce((acc, [_, isValid]) => acc && isValid, true);
   } catch {
     // nasty catch, muhahahaha
     return false;
@@ -65,9 +82,13 @@ const validatePassport = ({ byr, iyr, eyr, hgt, hcl, ecl, pid }: IPassport) => {
 
 const run = async () => {
   const inputArray = await fileToStringArray(FILE_PATH);
+
   const passports = passportsFromInputLines(inputArray);
-  const validPassports = passports.map(validatePassport).filter((x) => x);
-  console.log(validPassports.length);
+  const validPassportsFirst = passports.map(validatePassportFirst).filter((x) => x);
+  const validPassportsSecond = passports.map(validatePassportSecond).filter((x) => x);
+
+  console.log(validPassportsFirst.length);
+  console.log(validPassportsSecond.length);
 };
 
 run();
